41556bdea3cbf3803be7258f1bca99ad7c1661b6
Adam Kasabula <adam.kasabula@softwarehut.pl>
2017-06-02 13:44:40 +0200

    typings fixes

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.ts$/,
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader?keepUrl=true'],
+                loaders: ['ts', 'angular2-template-loader?keepUrl=true'],
                 exclude: [/node_modules/, /\.(spec|e2e)\.ts$/]
             }, {
                 test: /\.(html|css)$/,
